7886bb0f4b0a171069205438a913ec95d7c34b7c
chunjunghee <dkddkd0311@naver.com>
2014-09-11 16:01:08 +0900

    06

--- sketch.js
+++ sketch.js
@@ -1,26 +1,25 @@
 function setup() {
   createCanvas(640, 480);
-  background(128, 0, 64);
-  noFill()
   
+  noFill();
+
   stroke(255, 0, 0);
-  
-/*
-  var x;
 
-  x = 100;9
-  ellipse(x * 150, 100, 50, 50);
-
-  x = 250;
-  ellipse(x * 150, 100, 50, 50);
-
-  x = 300;
-  ellipse(x * 150, 100, 50, 50);
-  */
-
-
-function draw() {
-  background(128, 0, 64);
-
-  ellipse(320, 240, mouseX, mouseY);
 }
+
+function draw () {
+
+   background(128, 0, 64);
+
+for (var x = 1; x <= 8; x++)
+{
+  ellipse(x * 70, x * 20 + 100, x+50, x*10);
+}
+
+var size =    Math.abs(mouseX - 320);
+
+ellipse(320, 240, mouseX, mouseY);
+
+}
+
+  
